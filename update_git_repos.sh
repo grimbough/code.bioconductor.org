1337621689b0bf3bc67babcbffdb41a1e705ec50
Mike Smith <grimbough@gmail.com>
2020-08-04 23:37:57 +0200

    remove empty repos after cloning

--- update_git_repos.sh
+++ update_git_repos.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 BRANCHES=("RELEASE_3_11" "master")
-DIR=/tmp/repositories/
+#DIR=/tmp/repositories/
+DIR=/home/mike/repositories/
 
-ssh -i "xps_key" git@git.bioconductor.org info | grep -e "packages" | cut -f 2 | tail -n +3 | cut -f 2 -d "/" | head -n 50 > /tmp/packages.txt
+ssh -i "$HOME/.ssh/xps_key" git@git.bioconductor.org info | grep -e "packages" | cut -f 2 | tail -n +3 | cut -f 2 -d "/" | head -n 50 > /tmp/packages.txt
 
 mkdir -p ${DIR}
 cd ${DIR}
@@ -17,7 +18,16 @@ while read PACK; do
 		do
 			git checkout $BRANCH
 		done
+		## find the number of lines in the git log
+		tmp=`git log | wc -l`
 		cd ../
+
+		## some repos are empty, just delete them
+		if [ "$tmp" -eq 0 ] 
+		then
+		    rm -r ${PACK}
+		fi
+
 	else
         cd $PACK
         git remote update
