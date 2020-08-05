cb06137c4f20b5862530b469a9c55f592202feb1
Mike Smith <grimbough@gmail.com>
2020-08-05 17:01:32 +0200

    update scripts for maintaining git repos

---
+++ create_hound_config.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo -n "creating Hound config... "
+
+TMPFILE=/tmp/config.tmp
+OUTPUT="$HOME/bioc-code-tools/hound/config.json"
+REPOS="$HOME/repositories"
+
+echo -e "{
+\t\"max-concurrent-indexers\" : 2,
+\t\"dbpath\" : \"data\",
+\t\"title\" : \"Hound\",
+\t\"health-check-uri\" : \"/healthz\",
+\t\"repos\" : {" > "${TMPFILE}"
+
+for dir in "$REPOS"/*/     # list directories in the form "/tmp/dirname/"
+do
+    dir=${dir%*/}      # remove the trailing "/"
+    echo -e "\t\t\"${dir##*/}\" : {" >> "${TMPFILE}" 
+    echo -e "\t\t\t\"url\" : \"file:///${dir}\"" >> "${TMPFILE}"
+    echo -e "\t\t}," >> "${TMPFILE}"
+done
+
+readarray -t a < "${TMPFILE}"
+
+## remove temp file and output if it exists
+rm "${TMPFILE}"
+if [[ -f "${OUTPUT}" ]]; then
+    rm "${OUTPUT}"
+fi
+
+## write all but last line of out temp to outfile
+for ((i=0; i<${#a[@]}-1; i++)); do
+    echo "${a[$i]}" >> "${OUTPUT}"
+done
+
+echo "done"
+
+exit
+
+echo -e "\t\t}\n\t}\n}" >> "${OUTPUT}"
+
--- update_git_repos.sh
+++ update_git_repos.sh
@@ -4,33 +4,91 @@ BRANCHES=("RELEASE_3_11" "master")
 #DIR=/tmp/repositories/
 DIR=/home/mike/repositories/
 
-ssh -i "$HOME/.ssh/xps_key" git@git.bioconductor.org info | grep -e "packages" | cut -f 2 | tail -n +3 | cut -f 2 -d "/" | head -n 50 > /tmp/packages.txt
+touch "$DIR/ignored_packages.txt"
+readarray -t ignored < "$DIR/ignored_packages.txt"
+
+echo -n "Acquiring list of packages... "
+ssh -i "$HOME/.ssh/xps_key" git@git.bioconductor.org info | grep -e "packages" | cut -f 2 | tail -n +3 | cut -f 2 -d "/" | head -n 57 > /tmp/packages.txt
+echo "done"
 
 mkdir -p ${DIR}
 cd ${DIR}
 while read PACK; do
-    echo ${PACK}
+
+    echo "Package: ${PACK}"
+
+    ## check if we've added this package to our ignore list
+    if [[ " ${ignored[@]} " =~ " ${PACK} " ]]; then
+        echo "  Found in ignore list... skipped"
+	    continue
+    fi
+
 	if [[ ! -d "${PACK}" ]]
 	then
-		git clone "https://git.bioconductor.org/packages/${PACK}"
+	    
+	    echo -n "  Cloning repository... "
+		git clone --quiet "https://git.bioconductor.org/packages/${PACK}" > /dev/null
+		echo "done"
+		
+		echo -n "  Checking out branches... "
 		cd "${PACK}"
 		for BRANCH in ${BRANCHES[@]}
 		do
-			git checkout $BRANCH
+		    git checkout --quiet "$BRANCH" > /dev/null
 		done
+		echo "done"
+
 		## find the number of lines in the git log
-		tmp=`git log | wc -l`
+		## we will remove empty repos with 0 commits
+		ncommits=`git log | wc -l`
 		cd ../
 
+		## find the size of the downloaded repo
+		## we will remove any that are too large
+		dirsize=`du -s "${PACK}" | cut -f1`
+
 		## some repos are empty, just delete them
-		if [ "$tmp" -eq 0 ] 
+		if [ "$ncommits" -eq 0 ] 
 		then
-		    rm -r ${PACK}
+		    echo -n "  Empty repository... "
+		    rm -r "${PACK}"
+		    echo "${PACK}" >> ignored_packages.txt
+		    echo "removed"
+            continue
+		fi
+
+		if [[ "$dirsize" -gt 150000 ]]
+		then
+		    echo -n "  Repository too large... "
+		    echo "${PACK}" >> ignored_packages.txt
+		    rm -r "${PACK}"
+		    echo "removed"
+            continue
 		fi
 
 	else
         cd $PACK
-        git remote update
+        echo "  Repository already exists"
+        for BRANCH in ${BRANCHES[@]}
+        do
+            remote=`git ls-remote origin $BRANCH | cut -f 1`
+            local=`git rev-parse $BRANCH`
+
+            if [[ "$remote" != "$local" ]]
+            then
+                echo -n "  Updating $BRANCH branch... "
+                git checkout $BRANCH 
+	            git pull origin $BRANCH
+	            echo "done"
+            fi
+        done
+        
+        ## finish on the master branch
+        curbranch=`git rev-parse --abbrev-ref HEAD`
+        if [[ "$curbranch" != "master" ]]; then
+            git checkout master
+        fi
+        
 		cd ../
 	fi
 done </tmp/packages.txt
